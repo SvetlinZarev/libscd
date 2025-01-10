1c94820858aaf9ddf1459c506cb27d50c63a8ce4
Svetlin Zarev <svetlin@zarev.net>
2025-01-11 00:35:36 +0200

    update the SCD4x blocking example to the latest embassy version (0.7)

--- examples/embassy-scd4x/src/main.rs
+++ examples/embassy-scd4x/src/main.rs
@@ -1,40 +1,23 @@
 #![no_std]
 #![no_main]
+#![deny(warnings)]
 
 use defmt::*;
 #[allow(unused)]
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_stm32::dma::NoDma;
 use embassy_stm32::i2c::I2c;
 use embassy_stm32::time::Hertz;
-use embassy_stm32::{bind_interrupts, i2c, peripherals};
 use embassy_time::Delay;
 use embedded_hal::delay::DelayNs;
 use libscd::synchronous::scd4x::Scd4x;
 #[allow(unused)]
 use panic_probe as _;
 
-bind_interrupts!(struct Irqs {
-    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
-    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
-});
-
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-
-    let i2c = I2c::new(
-        p.I2C2,
-        p.PB10,
-        p.PB3,
-        Irqs,
-        NoDma,
-        NoDma,
-        Hertz(100_000),
-        Default::default(),
-    );
-
+    let i2c = I2c::new_blocking(p.I2C2, p.PB10, p.PB3, Hertz(100_000), Default::default());
     let mut scd = Scd4x::new(i2c, Delay);
 
     // When re-programming, the controller will be restarted,
